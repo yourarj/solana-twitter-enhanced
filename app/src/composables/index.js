b8aa0ed517a82378ed64432dfd828e46e01012b9
Awesome <dev>
2022-02-01 10:51:08 +0530

    build: removed unused app content
    Signed-off-by: Awesome
    
     Date:      Tue Feb 1 10:51:08 2022 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   README.md
    	deleted:    app/.gitignore
    	deleted:    app/README.md
    	deleted:    app/babel.config.js
    	deleted:    app/jsconfig.json
    	deleted:    app/package.json
    	deleted:    app/postcss.config.js
    	deleted:    app/public/favicon.ico
    	deleted:    app/public/index.html
    	deleted:    app/src/App.vue
    	deleted:    app/src/api/fetch-tweets.js
    	deleted:    app/src/api/get-tweet.js
    	deleted:    app/src/api/index.js
    	deleted:    app/src/api/send-tweet.js
    	deleted:    app/src/assets/logo.png
    	deleted:    app/src/components/PageHome.vue
    	deleted:    app/src/components/PageNotFound.vue
    	deleted:    app/src/components/PageProfile.vue
    	deleted:    app/src/components/PageTopics.vue
    	deleted:    app/src/components/PageTweet.vue
    	deleted:    app/src/components/PageUsers.vue
    	deleted:    app/src/components/TheSidebar.vue
    	deleted:    app/src/components/TweetCard.vue
    	deleted:    app/src/components/TweetForm.vue
    	deleted:    app/src/components/TweetList.vue
    	deleted:    app/src/components/TweetSearch.vue
    	deleted:    app/src/composables/index.js
    	deleted:    app/src/composables/useAutoresizeTextarea.js
    	deleted:    app/src/composables/useCountCharacterLimit.js
    	deleted:    app/src/composables/useFromRoute.js
    	deleted:    app/src/composables/useSlug.js
    	deleted:    app/src/main.css
    	deleted:    app/src/main.js
    	deleted:    app/src/routes.js
    	deleted:    app/tailwind.config.js
    	deleted:    app/vue.config.js
    	deleted:    app/yarn.lock

--- app/src/composables/index.js
+++
@@ -1,4 +0,0 @@
-export * from './useAutoresizeTextarea'
-export * from './useCountCharacterLimit'
-export * from './useFromRoute'
-export * from './useSlug'
--- app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
