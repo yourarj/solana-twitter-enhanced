1f6ba5a56c3b5509553e3d7de8c4cf7c011c2e72
yourarj <yourarj@gmail.com>
2022-04-16 13:00:52 +0530

    doc(program): document root lib

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,3 +1,15 @@
+//! Twitter 🐤 alternative built on top on
+//! of solana blockchain
+//!
+//! This project aims at mitigation of problems
+//! revolving around microbloging giant
+//! giving back control to users
+//! where users can freely open up and share
+//! whatever they want without the fear of sensorship
+//!
+//! @author: Arjun.
+//! contact: twitter.com/yourarj
+
 pub mod constants;
 pub mod errors;
 pub mod instructions;
@@ -9,10 +21,22 @@ use instructions::send_tweet::*;
 
 declare_id!("DheWr3dFgW3D2y6RL1CaWxG33Hnp9kvpoJXEedseVpLu");
 
+/// ## program module
+///
+/// contains all extrinsic method to interact with smart contract
 #[program]
 pub mod solana_twitter {
 
     use super::*;
+
+    /// ## Send tweet
+    ///
+    /// Send the tweet method
+    ///
+    /// - space_required - space intended to be occupied
+    /// - topic - topic of the tweet
+    /// - content - tweet body
+    ///
     pub fn send_tweet(
         ctx: Context<SendTweet>,
         space_required: u32,
@@ -22,6 +46,9 @@ pub mod solana_twitter {
         instructions::send_tweet::send_tweet(ctx, space_required, topic, content)
     }
 
+    /// ### Delete Tweet
+    ///
+    /// Delete the tweet specified by the account in request
     pub fn delete_tweet(ctx: Context<DeleteTweet>) -> Result<()> {
         instructions::delete_tweet::delete_tweet(ctx)
     }
